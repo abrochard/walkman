36de4aab15f8525205288705cdd926fe0a088af4
Adrien Brochard <aaa.brochard@gmail.com>
2020-11-23 15:17:48 -0500

    adding circleci config & make.el from kubel

---
+++ .circleci/make.el
@@ -0,0 +1,35 @@
+(require 'package)
+
+(defconst make-packages
+  '(org transient))
+
+(defun make-init ()
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (package-initialize))
+
+(defun make-install-packages ()
+  (make-init)
+  (package-refresh-contents)
+  (package-install 'package-lint)
+  (dolist (pkg make-packages)
+    (package-install pkg)))
+
+(defun make-ert ()
+  (make-init)
+  (load-file "/walkman/walkman.el")
+  (load-file "/walkman/test/walkman-test.el")
+  (ert-run-tests-batch-and-exit))
+
+(defun make-compile ()
+  (make-init)
+  (setq byte-compile-error-on-warn nil)
+  (batch-byte-compile))
+
+(defun make-lint ()
+  (make-init)
+  (require 'package-lint)
+  (setq package-lint-batch-fail-on-warnings t)
+  (package-lint-batch-and-exit))
+
+(provide 'make)
+;;; make.el ends here
